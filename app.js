445645e804c118ec175165afabb7435324f8d5fb
Jina Zhu <jinazhu87@gmail.com>
2020-05-27 10:52:12 -0700

    added animation and sound for pads

--- app.js
+++ app.js
@@ -1,14 +1,68 @@
 class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
+    this.playBtn = document.querySelector(".play");
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     //track
     this.index = 0;
+    this.bpm = 150;
+    this.isPlaying = null;
+  }
+  // click will add a class of active
+  activePad() {
+    this.classList.toggle("active");
   }
   repeat() {
     //when we arrive at 8, step will be 0 again, creating a loop
     let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    //loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      // check if pad is active
+      if (bar.classList.contains("active")) {
+        //check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
+    this.index++;
+  }
+  start() {
+    // arrow function to keep outer this
+    // run repeat
+    const interval = (60 / this.bpm) * 1000;
+    // check if it's playing
+    if (!this.isPlaying) {
+      this.isPlaying = setInterval(() => {
+        this.repeat();
+      }, interval);
+    }
   }
 }
+
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+  // when animation ends, repeat
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
+
+drumKit.playBtn.addEventListener("click", function () {
+  drumKit.start();
+});
